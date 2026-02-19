f3a54024073c4c6c19024877fdf41d64cc5e590a
hansol65 <whgksthf65@gmail.com>
2026-02-19 11:05:21 +0900

    style: 푸터 문의 배치 반응형

--- src/components/landing/SiteFooter.tsx
+++ src/components/landing/SiteFooter.tsx
@@ -4,7 +4,7 @@ export default function SiteFooter() {
       <div className="layout-desktop ">
         <div className="text-center text-h1">약관내용</div>
         <div className="h-px w-full bg-gray-200 my-5" />
-        <div className="md:text-caption text-[12px] text-gray-200 flex flex-col md:justify-between ">
+        <div className="flex flex-col md:flex-row md:justify-between md:text-caption text-[12px] text-gray-200">
           <span>서비스 문의 : 010-5121-6422 • hohyun.kim@likelion.net</span>
           <span>ⓒ 2026 멋쟁이사자처럼. All rights reserved.</span>
         </div>
