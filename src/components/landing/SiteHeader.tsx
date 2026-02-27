fa1529ef72e2bea47198076150f63957dff0aa89
hansol65 <whgksthf65@gmail.com>
2026-02-27 09:32:50 +0900

    style: 상단헤더 로고 추가

--- src/components/landing/SiteHeader.tsx
+++ src/components/landing/SiteHeader.tsx
@@ -1,3 +1,5 @@
+import Logo from "../../assets/image-logo-main.png";
+
 type SiteHeaderProps = {
   onOpenContact: () => void;
 };
@@ -7,7 +9,7 @@ export default function SiteHeader({ onOpenContact }: SiteHeaderProps) {
     <header className="fixed left-0 right-0 top-0 z-50 border-b border-white/5 bg-white/5 backdrop-blur-[25px]">
       <div className="mx-auto flex h-14 w-full max-w-[1440px] items-center justify-between px-4 sm:px-6 md:h-16 md:px-10 min-[1025px]:px-20">
         <div className="flex items-center">
-          <span className="h-3 w-12 rounded bg-white md:h-4 md:w-16"></span>
+          <img src={Logo} className="h-5 md:h-10"></img>
         </div>
 
         <button
